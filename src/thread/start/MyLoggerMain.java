6436b523e3288cac919d4b45ed41df4e72ad79d3
YjNoh <youngjean.noh@riman.com>
2025-03-31 14:38:45 +0900

    Lesson: Making Logger

---
+++ src/thread/start/MyLoggerMain.java
@@ -0,0 +1,14 @@
+package thread.start;
+
+import util.MyLogger;
+
+import static util.MyLogger.*;
+
+public class MyLoggerMain {
+
+    public static void main(String[] args) {
+
+        log("Hello Thread!");
+    }
+
+}
