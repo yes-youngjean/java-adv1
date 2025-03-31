916924eb0a8fba315f9f5b84ef7735710da5656d
YjNoh <youngjean.noh@riman.com>
2025-03-31 14:53:28 +0900

    Lesson: Practice make many threads

--- src/thread/start/HelloRunnable.java
+++ src/thread/start/HelloRunnable.java
@@ -1,9 +1,8 @@
 package thread.start;
 
 public class HelloRunnable implements Runnable {
-
     /**
-     * 실무에서는 하기 방식을 사용함
+     * 실무에서는 implements Runnable 방식을 사용함
      * */
     @Override
     public void run() {
