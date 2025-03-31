b2760113725cdfb28552e8d8c9868535db2df479
YjNoh <youngjean.noh@riman.com>
2025-03-31 14:03:58 +0900

    Lesson: Implement Runnable

---
+++ src/thread/start/HelloRunnable.java
@@ -0,0 +1,12 @@
+package thread.start;
+
+public class HelloRunnable implements Runnable {
+
+    /**
+     * 실무에서는 하기 방식을 사용함
+     * */
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": run()");
+    }
+}
